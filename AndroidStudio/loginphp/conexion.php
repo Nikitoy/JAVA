85799020f30acd04f2bece4a494155daca9a44fe
NizuBW <108291627+NizuBW@users.noreply.github.com>
2022-08-12 21:17:27 -0300

    Update conexion.php

--- AndroidStudio/loginphp/conexion.php
+++ AndroidStudio/loginphp/conexion.php
@@ -1,40 +1,13 @@
-<?PHP
-$hostname="localhost";
-$database="dbmoria";
-$username="";
-$password="";
-$json=array();
-    if(isset($_GET["username"]) && isset($_GET["password"])){
-        $username=$_GET['username'];
-        $password=$_GET['password'];
+<?php
 
-        $conexion=mysqli_connect($hostname,"moriausr","moriapwd",$database);
-        /*SELECT username, password FROM login WHERE username= "borja" AND password = "12345"*/
-        $consulta="SELECT username, password FROM login WHERE username= '{$username}' AND password = '{$password}'";
-        $resultado=mysqli_query($conexion,$consulta);
+    $mysql = new mysqli("localhost", "root", "", "android_users");
 
-        if($consulta){
-
-            if($reg=mysqli_fetch_array($resultado)){
-                $json['datos'][]=$reg;
-            }
-            mysqli_close($conexion);
-            echo json_encode($json);
-        }
-        else{
-            $results["username"]='';
-            $results["password"]='';
-            $json['datos'][]=$results;
-            echo json_encode($json);
-
-        }
-
+    if($mysql->connect_error)
+    {
+        die("fallo la conexion");
     }
-    else{
-            $results["username"]='';
-            $results["password"]='';
-            $json['datos'][]=$results;
-            echo json_encode($json);
+    else
+    {
+        echo "conexion satisfactoria";
+    }
 
-        }
-?>
