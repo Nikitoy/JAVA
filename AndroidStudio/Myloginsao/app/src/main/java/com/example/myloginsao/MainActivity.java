7b9a874fd11338a814ccb4d053acb4a1d9b8e379
NizuBW <108291627+NizuBW@users.noreply.github.com>
2022-09-30 21:44:44 -0300

    modificanding

--- AndroidStudio/Myloginsao/app/src/main/java/com/example/myloginsao/MainActivity.java
+++ AndroidStudio/Myloginsao/app/src/main/java/com/example/myloginsao/MainActivity.java
@@ -21,60 +21,49 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
-
-    private EditText etEmail, etPassword;
-    private String email, password;
-    private String URL = "http://10.0.2.2/login/login.php";
-
+    private static final String TAG = MainActivity.class.getName();
+    private Button btnRequest;
+    private Button btnSearch;
+    private EditText edName;
+    
+    private RequestQueue mRequestQueue;
+    private StringRequest mSringRequest;
+    //private String url = "http://headers.jsontest.com/";
+    private String url = "urlpostman";//"https://run.mocky.io/v3/b645788a-cbda-4d74-9780-75b0344ccbfb";
+    
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        email = password = "";
-        etEmail = findViewById(R.id.etEmail);
-        etPassword = findViewById(R.id.etPassword);
-    }
-
-    public void login(View view) {
-        email = etEmail.getText().toString().trim();
-        password = etPassword.getText().toString().trim();
-        if(!email.equals("") && !password.equals("")){
-            StringRequest stringRequest = new StringRequest(Request.Method.POST, URL, new Response.Listener<String>() {
-                @Override
-                public void onResponse(String response) {
-                    Log.d("res", response);
-                    if (response.equals("success")) {
-                        Intent intent = new Intent(MainActivity.this, Success.class);
-                        startActivity(intent);
-                        finish();
-                    } else if (response.equals("failure")) {
-                        Toast.makeText(MainActivity.this, "Invalid Login Id/Password", Toast.LENGTH_SHORT).show();
-                    }
-                }
-            }, new Response.ErrorListener() {
-                @Override
-                public void onErrorResponse(VolleyError error) {
-                    Toast.makeText(MainActivity.this, error.toString().trim(), Toast.LENGTH_SHORT).show();
-                }
-            }){
-                @Override
-                protected Map<String, String> getParams() throws AuthFailureError {
-                    Map<String, String> data = new HashMap<>();
-                    data.put("email", email);
-                    data.put("password", password);
-                    return data;
-                }
-            };
-            RequestQueue requestQueue = Volley.newRequestQueue(getApplicationContext());
-            requestQueue.add(stringRequest);
-        }else{
-            Toast.makeText(this, "Fields can not be empty!", Toast.LENGTH_SHORT).show();
+        
+        edName = (EditText) findViewById(R.id.edtTextName);
+        
+        btnSearch = (Button) findViewById(R.id.btnSearch);
+        
+        btnSearch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) { sendAndRequestResponse();}
         }
+    );
+        btnRequest = (Button) findViewById(R.id.buttonRequest);
+        
+        btnRequest.setOnClickListener(new View.OnclickListener() {
+            @Override
+            public void onClick(View v) { sendAndRequestResponse();}
+        }
+     );
     }
-
-    public void register(View view) {
-        Intent intent = new Intent(this, Register.class);
-        startActivity(intent);
-        finish();
-    }
+    private void sendAndRequestResponse() {
+        
+        //RequestQueue initialized
+        mRequestQueue = Volley.newRequestQueue(this);
+        Toast.makeText(getApplicationContext(),"manda", Toast.LENGTH_LONG).show();
+        mStringRequest = new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                Log.e("HttpClient", "success! response: " + response.toString());
+                Toast.makeText(getApplicationContext(), "Response :" + response.toString(), Toast.LENGHT_LONG).show();
+            }
+        
+                
 }
